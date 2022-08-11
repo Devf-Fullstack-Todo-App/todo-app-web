b1dbc4717fdf3ea098c697438fff62ef5565cb55
Misael Calvillo Mancilla <misa@homely.mx>
2022-08-10 20:18:26 -0500

    Crear tarea

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState('');
+
+  const handleSubmitTask = () => {
+    const body = {
+      todo: todo
+    }
+  
+    const config = {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+  
+    fetch('http://localhost:8000/todos', config)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setTodo('');
+        })
+        .catch((err) => console.error(err));
+  }
+
+  console.log('Se renderiza')
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{
+          backgroundColor: 'white',
+          borderRadius: '5px',
+        }}>
+          <input 
+            value={todo}
+            type="text" 
+            placeholder='Ingresa la tarea' 
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="button" onClick={handleSubmitTask}>Agregar tarea</button>
+        </div>
       </header>
     </div>
   );
