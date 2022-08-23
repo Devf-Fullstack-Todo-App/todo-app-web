86c90773b02321fcc8d87f23a64835bfff400cba
Luis Lechuga <luislechu55@gmail.com>
2022-08-22 20:03:32 -0500

    agrega componente de registro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [todo, setTodo] = useState('');
+  const [todo, setTodo] = useState("");
 
   const handleSubmitTask = () => {
     const body = {
@@ -10,23 +10,23 @@ function App() {
     }
   
     const config = {
-        method: 'POST', 
+        method: "POST", 
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(body)
     }
   
-    fetch('http://localhost:8000/todos', config)
+    fetch("http://localhost:8000/todos", config)
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
-          setTodo('');
+          setTodo("");
         })
         .catch((err) => console.error(err));
   }
 
-  console.log('Se renderiza')
+  console.log("Se renderiza")
 
   // 1. Obtener info del usuario 
   // 2. Registrar al usuario
@@ -37,42 +37,43 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='SignUp'>
+        <div className="SignUp">
 
           <form className="SignUp-form">
             <h1 className="SignUp-title">Registrate</h1>
             <input
             className="SignUp-input"
-            placeholder='Ingresa tu email'
+            placeholder="Ingresa tu email"
             type="email"
-            onChange={() => console.log('on mail change')}
+            onChange={() => console.log("on mail change")}
             />
 
             <input
             className="SignUp-input"
-            placeholder='Ingresa tu password'
+            placeholder="Ingresa tu password"
             type="password"
-            onChange={() => console.log('on password change')}
+            onChange={() => console.log("on password change")}
             />
 
             <input
             className="SignUp-input"
-            placeholder='Ingresa tu nombre'
+            placeholder="Ingresa tu nombre"
             type="name"
-            onChange={() => console.log('on name change')}
+            onChange={() => console.log("on name change")}
             />
 
             <input
             className="SignUp-input"
-            placeholder='Ingresa tu telefono'
+            placeholder="Ingresa tu telefono"
             type="phone"
-            onChange={() => console.log('on phone change')}
+            onChange={() => console.log("on phone change")}
             />
 
 
             <button
-            className='SignUp-bottom'
-            onClick={() => console.log('onRegister')}>
+            type="button"
+            className="SignUp-bottom"
+            onClick={() => console.log("onRegister")}>
               Registrarse
               </button>
           </form>
@@ -80,7 +81,7 @@ function App() {
           {/* <input 
             value={todo}
             type="text" 
-            placeholder='Ingresa la tarea' 
+            placeholder="Ingresa la tarea" 
             onChange={(e) => setTodo(e.target.value)}
           />
           <button type="button" onClick={handleSubmitTask}>Agregar tarea</button> */}
