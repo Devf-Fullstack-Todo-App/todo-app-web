30865e860dd93eacc09680ec4e6e839ffa573cac
Luis Lechuga <luislechu55@gmail.com>
2022-08-22 20:00:36 -0500

    agrega componente de registro

--- src/App.js
+++ src/App.js
@@ -28,20 +28,63 @@ function App() {
 
   console.log('Se renderiza')
 
+  // 1. Obtener info del usuario 
+  // 2. Registrar al usuario
+  // 3. Recibir respuesta y guardar el token
+  // 4. Utilizar ese token para crear una tarea de ese mismo usuario
+  // 4.1 - Necesitamos un endpoint "limitado" para ese usuario
+  
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{
-          backgroundColor: 'white',
-          borderRadius: '5px',
-        }}>
-          <input 
+        <div className='SignUp'>
+
+          <form className="SignUp-form">
+            <h1 className="SignUp-title">Registrate</h1>
+            <input
+            className="SignUp-input"
+            placeholder='Ingresa tu email'
+            type="email"
+            onChange={() => console.log('on mail change')}
+            />
+
+            <input
+            className="SignUp-input"
+            placeholder='Ingresa tu password'
+            type="password"
+            onChange={() => console.log('on password change')}
+            />
+
+            <input
+            className="SignUp-input"
+            placeholder='Ingresa tu nombre'
+            type="name"
+            onChange={() => console.log('on name change')}
+            />
+
+            <input
+            className="SignUp-input"
+            placeholder='Ingresa tu telefono'
+            type="phone"
+            onChange={() => console.log('on phone change')}
+            />
+
+
+            <button
+            className='SignUp-bottom'
+            onClick={() => console.log('onRegister')}>
+              Registrarse
+              </button>
+          </form>
+
+          {/* <input 
             value={todo}
             type="text" 
             placeholder='Ingresa la tarea' 
             onChange={(e) => setTodo(e.target.value)}
           />
-          <button type="button" onClick={handleSubmitTask}>Agregar tarea</button>
+          <button type="button" onClick={handleSubmitTask}>Agregar tarea</button> */}
+
         </div>
       </header>
     </div>
