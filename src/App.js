2cb076336da371166a37eee0749afc0e1217759b
Luis Lechuga <luislechu55@gmail.com>
2022-08-23 19:22:46 -0500

    Modulariza codigo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 import SignUp from './components/SignUp';
-import AuthForm from './components/SignUp';
 
 function App() {
   const [todo, setTodo] = useState('')
